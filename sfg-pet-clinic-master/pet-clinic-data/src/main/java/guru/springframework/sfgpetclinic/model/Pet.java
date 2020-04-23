dfd8a86a255f239abd751fb85695dcae0770a38a
SantyNet <sabuitrago@latam.stefanini.com>
2020-04-23 15:50:56 -0500

    nuevos cambios

--- sfg-pet-clinic-master/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic-master/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Pet  {
+public class Pet extends BaseEntity {
 
 
 
--- sfg-pet-clinic-master/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ sfg-pet-clinic-master/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -3,10 +3,13 @@ package guru.springframework.sfgpetclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
+             
     }
 }
